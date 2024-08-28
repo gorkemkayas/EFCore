1961ad4919ab5ad4d2391342d06c9cc5539754f7
Görkem Kaya <gorkemkayas@hotmail.com>
2024-08-28 23:24:04 +0300

    product comment added.

--- EFCore.CodeFirst/DAL/Product.cs
+++ EFCore.CodeFirst/DAL/Product.cs
@@ -11,7 +11,7 @@ namespace EFCore.CodeFirst.DAL
 {
     public class Product
     {
-        
+        // comment test
         public int Id { get; set; }
 
         public string Name { get; set; }
